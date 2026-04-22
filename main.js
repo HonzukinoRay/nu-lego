8676d3e83efc5ef2528caa0d231ab58af5481fa1
HonzukinoRay <orangesbooks00@gmail.com>
2026-04-22 13:21:44 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 setTimeout(() => {
                     preloader.style.display = 'none';
                 }, 600);
-            }, 2500);
+            }, 3000);
         }
     });
     
